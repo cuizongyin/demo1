3f38c9a06493b76cdf41928e15480ec982361572
cuizongyin <18255121397@163.com>
2022-06-04 16:10:54 +0800

    master push test

--- src/main/java/com/anlian/demo1/bean/GitTest.java
+++ src/main/java/com/anlian/demo1/bean/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("哈哈4");
         System.out.println("master commit");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 
 }
