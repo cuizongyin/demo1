b90ebe09f9c6a6108b9372cf0225eddc109b9828
cuizongyin <18255121397@163.com>
2022-06-04 16:29:47 +0800

    master 4次提交

--- src/main/java/com/anlian/demo1/bean/GitTest.java
+++ src/main/java/com/anlian/demo1/bean/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("哈哈2");
         System.out.println("哈哈3");
         System.out.println("哈哈4");
+        System.out.println("master commit");
     }
 
 }
