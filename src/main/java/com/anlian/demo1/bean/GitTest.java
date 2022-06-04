3f9a92289239b600371896de800ea88b737c450c
cuizongyin <44997830+cuizongyin@users.noreply.github.com>
2022-06-04 16:54:46 +0800

    Update GitTest.java
    master pull test

--- src/main/java/com/anlian/demo1/bean/GitTest.java
+++ src/main/java/com/anlian/demo1/bean/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("master commit");
         System.out.println("hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 
 }
